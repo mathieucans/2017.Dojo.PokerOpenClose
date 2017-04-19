98c8f48babc104767f0935086e00662b1e6f88cb
CANS Mathieu <MC05359S@atlas.edf.fr>
2017-04-19 16:23:37 +0200

    red:match_if_hand_contains_two_pairs

--- PokerOpenClosed/SinglePairShould.cs
+++ PokerOpenClosed/SinglePairShould.cs
@@ -17,6 +17,26 @@ namespace PokerOpenClosed
 		}
 
 		[TestMethod]
+		public void match_if_hand_contains_three_same_cards()
+		{
+			var a_single_pair_hand = HandConverter.CreateHand("Kd Kd Kc 7s 8h");
+
+			var singlePair = new SinglePair();
+
+			Check.That(singlePair.Match(a_single_pair_hand)).IsFalse();
+		}
+
+		[TestMethod]
+		public void match_if_hand_contains_two_pairs()
+		{
+			var a_single_pair_hand = HandConverter.CreateHand("Kd Qd Kc 7s Qh");
+
+			var singlePair = new SinglePair();
+
+			Check.That(singlePair.Match(a_single_pair_hand)).IsFalse();
+		}
+
+		[TestMethod]
 		public void dosent_match_if_hand_contains_no_same_cards()
 		{
 			var hand = HandConverter.CreateHand("Kd Qd Jc Ts 7h");
